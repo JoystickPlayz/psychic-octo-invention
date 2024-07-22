74a7e16e84ac398adbe2b07feddeb960148fc272
JoystickPlayz <robloxgenerated1@gmail.com>
2024-07-22 22:59:25 +0300

    Create yarhm.lua

---
+++ yarhm.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
+loadstring(Game:HttpGet("https://pastebin.com/raw/W0sD2JwE"))()
